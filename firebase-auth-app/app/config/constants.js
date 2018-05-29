c329cbdbf5ddd0d481dfdeb8cccb56d3a6a5f28f
Lauren M Hale <laurenmhale@ool-ad020168.dyn.optonline.net>
2018-05-29 09:05:37 -0400

    adding firebase auth

---
+++ firebase-auth-app/app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyAov7E0XsOFQl4XkevMrgCo2W7WzCua53w' ;
+export const FIREBASE_AUTH_DOMAIN = 'booksapp-d9493.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://booksapp-d9493.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'booksapp-d9493';
+export const FIREBASE_STORAGE_BUCKET = 'booksapp-d9493.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '38597134316';
+
+export const FACEBOOK_APP_ID = '2052531854967808';
